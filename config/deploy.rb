10a7ee1f05106e52b661b08763bc6189d2aea92d
code-playmaker <s.ueno1229@gmail.com>
2020-07-05 15:38:23 +0900

    edit at deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.1"
 
 set :application, "fleamarket_sample_74e"
-set :repo_url, "git@example.com:code-playmaker/fleamarket_sample_74e.git"
+set :repo_url, "git@github.com:code-playmaker/fleamarket_sample_74e.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 

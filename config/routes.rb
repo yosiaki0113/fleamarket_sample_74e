dd4e0c101351d0ac1bfa495d817120cdb6f0faf6
yosiaki0113 <65077274+yosiaki0113@users.noreply.github.com>
2020-07-25 22:36:55 +0900

    Edit マークアップの実装用に修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :items, only: [:index, :new, :create, :show] do
+    collection do
+      get 'show_itemlist'
+    end
+  end
 end

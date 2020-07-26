0232bc4210545159c1099fdc18321cd8be83231a
code-playmaker <s.ueno1229@gmail.com>
2020-07-26 16:48:13 +0900

    【編集】ルーティング(routes.rb)
    ・（3行目）deviseのコントローラーがどれを使っているのかを明示するための記述。
    ・(4~7行)新規登録画面で、shipping_addressを登録できるように、ルーティングを設定。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'users/registrations'}
+
+  devise_scope :user do
+    get 'shipping_addresses', to: 'users/registrations#new_shipping_address'
+    post 'shipping_addresses', to: 'users/registrations#create_shipping_address'
+  end
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end

68a5d6859e4965831410ae6a4f45d1c84f5e3571
code-playmaker <s.ueno1229@gmail.com>
2020-07-26 16:19:53 +0900

    shipping_addressテーブルにカラムを追加

--- db/migrate/20200726071757_create_shipping_addresses.rb
+++ db/migrate/20200726071757_create_shipping_addresses.rb
@@ -1,7 +1,17 @@
 class CreateShippingAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :shipping_addresses do |t|
-
+      t.string :destination_family_name,          null: false
+      t.string :destination_first_name,           null: false
+      t.string :destination_family_name_kana,     null: false
+      t.string :destination_first_name_kana,      null: false
+      t.integer :post_code,                       null: false
+      t.integer :prefectures,                      null: false
+      t.string :city,                             null: false
+      t.string :house_number,                     null: false
+      t.string :building_name
+      t.integer :phone,                           null: false
+      t.references :user,                      foreign_key: true
       t.timestamps
     end
   end

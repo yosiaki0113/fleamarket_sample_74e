bcacebfeae5380923bfbbaf4391f91f49d88c8ce
code-playmaker <s.ueno1229@gmail.com>
2020-07-26 16:56:38 +0900

    【編集】コントローラー(users/registrations.controller.rb)
    ・デフォルトのコメントアウト文は消去。
    ・(3~6行目)新規登録画面の１ページ目を作成。user, profileモデルを登録できるようした。
    ・(8~13行目)１ページ目のデータを保持したまま、2ページ目でshipping_addressモデルを登録できるように設定。また、10~13行目の"valid？"メソッドを用いて1ページ目でバリデーションに引っ掛かったら画面遷移できないように設定。
    ・(20~30行目)全てのデータを登録できるように設定。また、23~26行目の"valid？"メソッドを用いて2ページ目でバリデーションに引っ掛かったら画面遷移できないように設定。
    ・(32行目~)ストロングパラメーター設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,62 +1,42 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    @user = User.new
+    @user.build_profile
+  end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    @user = User.new(user_params)
+    unless @user.valid?
+      flash.now[:alert] = @user.errors.full_messages
+      render :new and return
+    end
+    session["devise.regist_data"] = {user: @user.attributes}
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    @shipping_address = @user.shipping_addresses.build
+    render :new_shipping_address
+  end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def create_shipping_address
+    @user = User.new(session["devise.regist_data"]["user"])
+    @shipping_address = Shipping_address.new(address_params)
+    unless @shipping_address.valid?
+      flash.now[alert] = @shipping_address.errors.full_messages
+      render :new_profile and return
+    end
+    @user.build_shipping_address(@shipping_address.attributes)
+    @user.save!
+    sgin_in(:user, @user)
+  end
 
-  # PUT /resource
-  # def update
-  #   super
-  # end
+  private
 
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
+  def user_params
+    params.require(:user).permit(:nickname,:email,:password, :password_confirmation, profile_attributes:[:id, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day])
+  end
 
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def address_params
+    params.require(:shipping_address).permit(:destination_first_name, :destination_family_name, :destination_first_name_kana, :destination_family_name_kana, :post_code, :prefectures, :city, :house_number, :building_name, :phone)
+  end
+  
 end

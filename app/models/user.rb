0e5544f3e511a7f238f498e01fbf92bf41d960c9
code-playmaker <s.ueno1229@gmail.com>
2020-07-26 16:28:48 +0900

    【編集】userモデル
    ・追加した[:nickname]のバリデーション設定
    ・profile, shipping_addressモデルとのアソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true, uniqueness: true
+
+  has_one :profile, dependent: :destroy
+  accepts_nested_attributes_for :profile
+
+  has_many :shipping_addresses, dependent: :destroy
 end

108c2c27f3ae554c1c77e12fed5c2c5f89ab0a64
yosiaki0113 <65077274+yosiaki0113@users.noreply.github.com>
2020-08-12 00:11:58 +0900

    Edit: Routeを修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: :show
-  resource :items do
+  resources :items do
     collection do
       get 'show_itemlist'
       get 'purchase','done','card_add','card_new','card_done'

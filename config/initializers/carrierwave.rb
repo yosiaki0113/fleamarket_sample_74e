9127361d1f1399a95d20305e3303de8b38d313f9
code-playmaker <s.ueno1229@gmail.com>
2020-08-15 18:36:22 +0900

    [Edit] carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,8 +8,8 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1'
     }
-    config.fog_directory  = ‘code-playmaker’
-    config.asset_host = ‘https//s3-ap-northeast-1.amazonaws.com/code-playmaker’
+    config.fog_directory  = 'code-playmaker'
+    config.asset_host = 'https//s3-ap-northeast-1.amazonaws.com/code-playmaker'
   else
     config.storage :file
     config.enable_processing = false if Rails.env.test?

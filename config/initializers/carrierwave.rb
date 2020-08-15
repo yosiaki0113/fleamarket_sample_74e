603054ddeda496261d08007f5a437c615381eeaf
code-playmaker <s.ueno1229@gmail.com>
2020-08-15 18:38:34 +0900

    [Edit] carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.strage = :fog
+    config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',

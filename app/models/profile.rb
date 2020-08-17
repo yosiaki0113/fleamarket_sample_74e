34d1857374fa7e8a35bfb2fd02a3244daaf3cdcf
code-playmaker <s.ueno1229@gmail.com>
2020-08-17 19:48:29 +0900

    [Edit] profileモデルファイル

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,8 @@
 class Profile < ApplicationRecord
   belongs_to :user, optional: true
   validates :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, presence: true
+  validates :family_name, :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角漢字・ひらがなで入力してください"}
+  validates :birth_year, numericality: {only_integer: true, greater_than_or_equal_to: 1989, less_than_or_equal_to: 2020}
+  validates :birth_month, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 12}
+  validates :birth_day, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 31}
 end

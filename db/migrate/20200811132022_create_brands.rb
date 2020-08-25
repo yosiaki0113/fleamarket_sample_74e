6e8f946d8cb5ec4e31630e82e04e0523a37db9f5
suzuki0725 <los_campesinos1130@yahoo.co.jp>
2020-08-25 15:07:39 +0900

    Edit: 外部キー制約の追記

--- db/migrate/20200811132022_create_brands.rb
+++ db/migrate/20200811132022_create_brands.rb
@@ -2,6 +2,7 @@ class CreateBrands < ActiveRecord::Migration[5.2]
   def change
     create_table :brands do |t|
       t.string :name
+      t.references :item, foreign_key: true
       t.timestamps
     end
   end

9c7cb31a819e1afa8e14be27562b65e4d6c0c862
yosiaki0113 <65077274+yosiaki0113@users.noreply.github.com>
2020-08-27 01:19:55 +0900

    Update 20200811132722_add_references_to_items.rb

--- db/migrate/20200811132722_add_references_to_items.rb
+++ db/migrate/20200811132722_add_references_to_items.rb
@@ -1,6 +1,5 @@
 class AddReferencesToItems < ActiveRecord::Migration[5.2]
   def change
-    add_reference :items, :brand, foreign_key: true
     add_reference :items, :category, null:false, foreign_key: true
   end
 end

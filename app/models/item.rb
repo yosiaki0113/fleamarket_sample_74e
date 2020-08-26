1daf2b6abac97fcc1a572950bb3e5cf84a2a5195
suzuki0725 <los_campesinos1130@yahoo.co.jp>
2020-08-26 16:35:56 +0900

    エラーメッセージの文面変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,8 +11,8 @@ class Item < ApplicationRecord
   belongs_to :category
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
-  validates :images, 
-            :name, 
+  validates :images, presence: {message: 'がありません'}
+  validates :name, 
             :text,
             :price, 
             :condtion_id, 

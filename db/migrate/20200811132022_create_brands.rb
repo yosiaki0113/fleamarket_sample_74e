4814efc37d830b3df46091c767e621be455d4010
yosiaki0113 <65077274+yosiaki0113@users.noreply.github.com>
2020-08-27 01:20:10 +0900

    Delete 20200811132022_create_brands.rb
    ブランドテーブルを削除

--- db/migrate/20200811132022_create_brands.rb
+++
@@ -1,9 +0,0 @@
-class CreateBrands < ActiveRecord::Migration[5.2]
-  def change
-    create_table :brands do |t|
-      t.string :name
-      t.references :item, foreign_key: true
-      t.timestamps
-    end
-  end
-end

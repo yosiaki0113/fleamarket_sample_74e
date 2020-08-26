5081ccf8b7d84b91dbf784a72956a45324127420
yosiaki0113 <65077274+yosiaki0113@users.noreply.github.com>
2020-08-27 02:27:09 +0900

    Update toppage.js
    全商品のレイアウトを修正

--- app/assets/javascripts/toppage.js
+++ app/assets/javascripts/toppage.js
@@ -8,9 +8,9 @@
 //   })
 // }
 
-// $(function(){
-//   $('.itemlistmain__showbox__content__itemlists >li').each(function(index,element){
-//     var w = $(element).width() +10
-//     $('.itemlistmain__showbox__content__itemlists').append('<li style="height:0; width:'+ w +'px; margin:0;"></li>')
-//   })
-// })
+$(function(){
+  $('.itemlistmain__showbox__content__itemlists >li').each(function(index,element){
+    var w = $(element).width() +10
+    $('.itemlistmain__showbox__content__itemlists').append('<li style="height:0; width:'+ w +'px; margin:0;"></li>')
+  })
+})

6202b270de229002217188937c5f4282b229f897
code-playmaker <s.ueno1229@gmail.com>
2020-08-29 09:18:10 +0900

    [Edit] application.rb

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module FleamarketSample74e
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.available_locales = [:ja, :en]
     config.time_zone = 'Tokyo'
+    Rails.application.config.assets.unknown_asset_fallback = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

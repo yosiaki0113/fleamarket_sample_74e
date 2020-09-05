8e9344eaa3f438d0dec9d8d0d3077c66a0edabc3
code-playmaker <s.ueno1229@gmail.com>
2020-09-05 21:17:42 +0900

    [Edit] routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'likes/create'
   get 'likes/destroy'
-  devise_for :users, controllers: {registrations: 'users/registrations', sessions: 'users/sessions'}
+  devise_for :users, controllers: {registrations: 'users/registrations', sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks'}
 
   devise_scope :user do
     get 'shipping_addresses', to: 'users/registrations#new_shipping_address'

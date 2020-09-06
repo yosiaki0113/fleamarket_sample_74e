207637c3805f5352a2e2abc36791f0b598cd1a69
code-playmaker <s.ueno1229@gmail.com>
2020-09-06 13:31:27 +0900

    [Edit] credentials

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -309,4 +309,5 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   config.omniauth :google_oauth2,ENV['GOOGLE_CLIENT_ID'],ENV['GOOGLE_CLIENT_SECRET']
+  config.omniauth :google_oauth2,Rails.application.credentials[:google_oauth2][:GOOGLE_CLIENT_ID],Rails.application.credentials[:google_oauth2][:GOOGLE_CLIENT_SECRET]
 end

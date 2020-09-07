8efd10142176f312b6bc3ef3ce148bc2f5489524
andou-0307 <703515412307a@gmail.com>
2020-09-07 09:56:45 +0900

    【追加】テスト記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -56,4 +56,61 @@ describe Item, type: :model do
     end
 
   end
+
+  describe '#update' do
+    it "nameがない場合は登録できないこと" do
+      item = build(:item, name: "")
+      item.valid?
+      expect(item.errors[:name]).to include("を入力してください")
+    end
+
+    it "textがない場合は登録できないこと" do
+      item = build(:item, text: "")
+      item.valid?
+      expect(item.errors[:text]).to include("を入力してください")
+    end
+
+    it "priceがない場合は登録できないこと" do
+      item = build(:item, price: "")
+      item.valid?
+      expect(item.errors[:price]).to include("を入力してください")
+    end
+
+    it "item_condtion_idがない場合は登録できないこと" do
+      item = build(:item,condtion_id: "")
+      item.valid?
+      expect(item.errors[:condtion_id]).to include("を入力してください")
+    end
+
+    it "prefecture_idがない場合は登録できないこと" do
+      item = build(:item, prefecture_id: "")
+      item.valid?
+      expect(item.errors[:prefecture_id]).to include("を入力してください")
+    end
+
+    it "postage_type_idがない場合は登録できないこと" do
+      item = build(:item, postage_type_id: "")
+      item.valid?
+      expect(item.errors[:postage_type_id]).to include("を入力してください")
+    end
+
+    it "days_until_shipping_idがない場合は登録できないこと" do
+      item = build(:item, days_until_shipping_id: "")
+      item.valid?
+      expect(item.errors[:days_until_shipping_id]).to include("を入力してください")
+    end
+
+    it "imageがない場合は登録できないこと" do
+      item = build(:item, images: [])
+      item.valid?
+      expect(item.errors[:images]).to include("を入力してください")
+    end
+
+    it "category_idがない場合は登録できないこと" do
+      item = build(:item, category_id: "")
+      item.valid?
+      expect(item.errors[:category_id]).to include("を入力してください")
+    end
+
+  end
 end

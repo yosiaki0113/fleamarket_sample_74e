0a7447f514968b462960bdc7e901a3a1b6972d88
yosiaki0113 <65077274+yosiaki0113@users.noreply.github.com>
2020-09-10 23:48:14 +0900

    Create order_controller.rb
    商品購入機能のコントローラーを追加

---
+++ app/controllers/order_controller.rb
@@ -0,0 +1,42 @@
+class OrderController < ApplicationController
+  require 'payjp'
+  before_action :set_likes_items
+  def index
+    @item = Item.find(params[:item_id])
+    @image = Image.where(item_id: @item).first
+    @category = Category.find(@item.category_id)
+    @user = current_user
+    @address = current_user.shipping_addresses[0]
+    # binding.pry
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+      redirect_to controller: "card", action: "new"
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
+  end
+
+  def pay
+    @item = Item.find(params[:item_id])
+    card = Card.where(user_id: current_user.id).first
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    Payjp::Charge.create(
+    :amount => @item.price,
+    :customer => card.customer_id,
+    :currency => 'jpy', 
+  )
+    @item.update(purchase_date: Time.current)
+    @item.update(trading_status: "取引中")
+    @item.update(buyer_id: current_user.id)
+
+  redirect_to action: 'done' 
+  end
+
+  def set_likes_items
+    if user_signed_in?
+      @like_items = Item.where(id: current_user.likes.select("item_id"))
+    end
+  end
+end

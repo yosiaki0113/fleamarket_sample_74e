1866a3365fefd099fd15f6cdea1e2973a7f566e3
yosiaki0113 <65077274+yosiaki0113@users.noreply.github.com>
2020-09-10 23:51:03 +0900

    Update 20200908122314_create_cards.rb
    購入機能追加

--- db/migrate/20200908122314_create_cards.rb
+++ db/migrate/20200908122314_create_cards.rb
@@ -1,7 +1,7 @@
 class CreateCards < ActiveRecord::Migration[5.2]
   def change
     create_table :cards do |t|
-      t.integer :user_id, null: false
+      t.references :user,foreign_key: true,null:false
       t.string :customer_id, null: false
       t.string :card_id, null: false
       t.timestamps

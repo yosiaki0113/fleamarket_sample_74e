d4bcb42d9222b2fce698acf6e20326b5265fdde4
shosei666 <65118535+shosei666@users.noreply.github.com>
2020-09-11 21:30:22 +0900

    Update card_controller.rb

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -43,7 +43,7 @@ class CardController < ApplicationController
   end
 
   def show #Cardのデータpayjpに送り情報を取り出します
-    card = Card.where(user_id: current_user.id).first
+    card = find_by(user_id: current_user.id)
     if card.blank?
       redirect_to action: "index" 
     else
